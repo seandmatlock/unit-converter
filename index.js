3a2ef2d712a3436c7c14cd9d6656aaadd9bc18d4
seanmatlock <beeredbeard@gmail.com>
2022-11-24 13:50:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,11 @@ let length = document.getElementById("length-conversion")
 let volume = document.getElementById("volume-conversion")
 let mass = document.getElementById("mass-conversion")
 
+// Need to refactor this into template literals, but haven't figured it out yet 
+
 function lengthConversion() {
     length.textContent = inputEl.value + " meters = " + (inputEl.value * 3.281).toFixed(3)  + ' feet | ' + inputEl.value + " feet = " + (inputEl.value * 0.304).toFixed(3) + " meters."
 } 
-
-// function lengthConversion() {
-//     `${length.textContent} = ${inputEl.value} meters = ${(inputEl.value * 3.281).toFixed(3)} feet | ${inputEl.value} feet = ${(inputEl.value * 0.304).toFixed(3)} meters.`
-// } 
 
 function volumeConversion() {
     volume.textContent = inputEl.value + " liters = " + (inputEl.value * 0.264).toFixed(3)  + ' gallons | ' + inputEl.value + " gallons = " + (inputEl.value * 3.785).toFixed(3) + " liters."
